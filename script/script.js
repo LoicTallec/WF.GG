1f4e4ed10ed846f63732ba7012e282833a77c53d
Loïc Tallec <loictallec1@gmail.com>
2023-10-12 18:44:24 +0200

    Fix request and cookie

--- script/script.js
+++ script/script.js
@@ -11,41 +11,75 @@ function creatProfil() {
   window.location.href = "page2.html";
 }
 
-function saveSelection(selection) {
-  // Effectuer une action appropriée avec la sélection
-  // Par exemple, vous pouvez l'enregistrer dans le stockage local du navigateur
-  // ou l'envoyer au serveur via une requête AJAX.
+document.addEventListener('DOMContentLoaded', function() {
+  const badgeItems = document.querySelectorAll('.badge-item');
 
-  // Exemple d'enregistrement dans le stockage local du navigateur (localStorage) :
-  localStorage.setItem('selectedBadge', selection);
-  
-  // Ou exemple d'envoi au serveur via une requête AJAX (utilisation de la bibliothèque fetch) :
-  fetch('/votre-action', {
-    method: 'POST',
-    body: JSON.stringify({ selection: selection }),
-    headers: { 'Content-Type': 'application/json' }
-  })
-  .then(response => {
-    // Gérer la réponse du serveur si nécessaire
-    console.log('Sélection sauvegardée avec succès !');
-  });
-}
+  badgeItems.forEach((item) => {
+    const badgeText = item.textContent;
+    let clickCounter = getCookie(badgeText) || localStorage.getItem(badgeText) || 0;
 
-// Sélectionnez tous les éléments li avec la classe "badge-item"
-const badgeItems = document.querySelectorAll('.badge-item');
+    const counterSpan = document.createElement('span');
+    counterSpan.classList.add('counter');
+    counterSpan.textContent = clickCounter.toString();
+    item.appendChild(counterSpan);
 
-// Parcourez tous les éléments li
-badgeItems.forEach((item) => {
-  let clickCounter = 0; // Compteur de clics initial
-
-  // Créez un élément span pour afficher le compteur de clics
-  const counterSpan = document.createElement('span');
-  counterSpan.textContent = clickCounter.toString(); // Affichez le compteur de clics initial
-  item.appendChild(counterSpan); // Ajoutez le span à l'élément li
-
-  // Ajoutez un gestionnaire d'événements de clic à chaque élément li
-  item.addEventListener('click', () => {
-    clickCounter++; // Incrémentez le compteur de clics
-    counterSpan.textContent = clickCounter.toString(); // Mettez à jour le contenu du span avec le compteur de clics
+    item.addEventListener('click', () => {
+      if (!item.classList.contains('disabled')) {
+        clickCounter++;
+        counterSpan.textContent = clickCounter.toString();
+        item.classList.add('disabled');
+        saveSelection(item.textContent, clickCounter);
+        setCookie(item.textContent, clickCounter, 365); // Stocke la valeur du compteur dans un cookie pour une durée d'un an (365 jours)
+        localStorage.setItem(badgeText, clickCounter); // Enregistre la valeur du compteur dans le stockage local
+        setTimeout(() => {
+          item.classList.remove('disabled');
+        }, 10000);
+      }
+    });
   });
 });
+
+function setCookie(name, value, days) {
+  const expires = new Date();
+  expires.setTime(expires.getTime() + (days * 24 * 60 * 60 * 1000));
+  const cookie = `${name}=${encodeURIComponent(value)};expires=${expires.toUTCString()};path=/;SameSite=None;Secure`;
+  document.cookie = cookie;
+}
+
+function getCookie(name) {
+  const cookieName = `${name}=`;
+  const cookieArray = document.cookie.split(';');
+
+  for (let i = 0; i < cookieArray.length; i++) {
+    let cookie = cookieArray[i];
+    while (cookie.charAt(0) === ' ') {
+      cookie = cookie.substring(1);
+    }
+    if (cookie.indexOf(cookieName) === 0) {
+      return decodeURIComponent(cookie.substring(cookieName.length, cookie.length));
+    }
+  }
+  return null;
+}
+
+function saveSelection(badgeText, clickCount) {
+  // Votre code pour enregistrer la sélection du badge et le compteur de clics ici
+  const xhr = new XMLHttpRequest();
+  xhr.open('POST', 'url_de_votre_endpoint', true);
+  xhr.setRequestHeader('Content-Type', 'application/json');
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      console.log('Badge enregistré avec succès');
+    }
+  };
+  const data = JSON.stringify({ badge: badgeText, count: clickCount });
+  xhr.send(data);
+  // Enregistrement dans le stockage local du navigateur
+  localStorage.setItem('badge', badgeText);
+  localStorage.setItem('clickCount', clickCount);
+}
+
+// Récupération des valeurs du stockage local du navigateur
+const savedBadge = localStorage.getItem('badge');
+const savedClickCount = localStorage.getItem('clickCount');
+
